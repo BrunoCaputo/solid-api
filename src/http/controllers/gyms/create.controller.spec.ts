3989ecea56586eb92e3f02c67f9b786e907ca912
BrunoCaputo <brunopc.dev@gmail.com>
2025-02-17 21:18:13 -0300

    feat: Gyms controllers

--- src/http/controllers/gyms/create.controller.spec.ts
+++ src/http/controllers/gyms/create.controller.spec.ts
@@ -3,7 +3,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 import { app } from '@/app'
 
-describe('Register (e2e)', () => {
+describe('Create gym (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,7 +12,7 @@ describe('Register (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to register', async () => {
+  it('should be able to create a gym', async () => {
     const response = await request(app.server).post('/users').send({
       name: 'John Doe',
       email: 'johndoe@example.com',
