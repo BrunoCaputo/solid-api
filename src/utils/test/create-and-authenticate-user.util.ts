d0def95b67b7357083515b732d4151737fad0412
BrunoCaputo <brunopc.dev@gmail.com>
2025-02-22 23:51:52 -0300

    test: Added role validation to e2e tests

--- src/utils/test/create-and-authenticate-user.util.ts
+++ src/utils/test/create-and-authenticate-user.util.ts
@@ -1,5 +1,8 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
+
+import { prisma } from '@/lib/prisma'
 
 interface AuthenticatedUserResponse {
   token: string
@@ -7,11 +10,15 @@ interface AuthenticatedUserResponse {
 
 export async function createAndAuthenticateUser(
   app: FastifyInstance,
+  isAdmin = false,
 ): Promise<AuthenticatedUserResponse> {
-  await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    password: '123456',
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
